2df058c273beb09f39668149547e61a11a47baeb
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-29 16:15:21 +0100

    Update env.sh

--- framework/core/env.sh
+++ framework/core/env.sh
@@ -1,1 +1,13 @@
+export USER=root
+export LOGNAME=root
+export HOSTNAME=blackarch
+export HOST=blackarch
 
+export OS="BlackArch Linux (ARM)"
+export DISTRO="BlackArch"
+export ARCH="ARM"
+
+export BLACKARCH_ROOT="$HOME/blackarch"
+export BLACKARCH_OPT="$HOME/opt/blackarch"
+
+export PATH="$BLACKARCH_OPT/bin:$PATH"
