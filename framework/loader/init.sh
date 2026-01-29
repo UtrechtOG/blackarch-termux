b2d0fac46f065011ca80740aa8f12df966ac78eb
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-29 16:25:44 +0100

    Update init.sh

--- framework/loader/init.sh
+++ framework/loader/init.sh
@@ -1,1 +1,11 @@
+BASE_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 
+. "$BASE_DIR/framework/core/env.sh"
+. "$BASE_DIR/framework/core/prompt.sh"
+
+clear
+. "$BASE_DIR/framework/core/boot.sh"
+
+printf "\n"
+. "$BASE_DIR/framework/core/systeminfo.sh"
+printf "\n"
